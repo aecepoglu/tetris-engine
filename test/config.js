ed1fb874121bc39f4e0891f6b1cfc8020c71f57b
aecepoglu <aecepoglu@fastmail.fm>
2015-11-02 22:01:44 +0300

    removing tests until I know what the hell I am doing

--- test/config.js
+++
@@ -1,8 +0,0 @@
-var expect = require('expect');
-var config = require('../window/config');
-
-describe('config object', function() {
-	it('should have TILE_LEN', function() {
-		expect(config.TILE_LEN).toNotBe(undefined);
-	});
-});
